cf6ecd9bbed887b836166f254bcc5f71cea2ab0a
Vincent Bauchart <vincent.bauchart@laposte.net>
2008-10-17 13:38:49 +0000

    add POD

--- V0/Core/bin/PC_LIST_TAB.pl
+++ V0/Core/bin/PC_LIST_TAB.pl
@@ -1,11 +1,97 @@
 #!/usr/bin/perl
 
+# Inclusions obligatoires
 use strict;
+use Pod::Usage;
+use Getopt::Std;
 
+#  Documentation
+###########################################################
+=head1 NAME
+
+PC_LIST_TAB - Liste le contenu d'une table dans un environnement
+
+=head1 SYNOPSIS
+
+ PC_LIST_TAB.pl [-h][-v] environnement tablename
+ 
+=head1 DESCRIPTION
+
+Liste les champs d'une table dans un environnement à la date courante
+
+=head1 ENVIRONNEMENT
+
+=item ITOOLS : L'environnement du service de l'ICles IKOS doit être chargé
+
+=head1 OPTIONS
+
+=head2 -h : Affiche l'aide en ligne
+
+=head2 -v : Mode verbeux
+
+=head1 ARGUMENTS
+
+=head2 * environnement à utiliser
+
+=head2 * table a ouvrir
+
+=head1 AUTHOR
+
+BV Associates, 16/10/2008
+
+=cut
+
+
+#  Fonctions
+###########################################################
+
+sub sortie ($) {
+	exit shift;
+}
+
+sub usage($) {
+	my $verbosity=shift;
+	pod2usage(-verbose => $verbosity, -noperldoc => 1);
+	sortie(202); 
+}
+
+sub log_erreur {
+	print STDERR "ERREUR: ".join(" ",@_)."\n"; 
+	sortie(202);
+}
+
+sub log_info {
+	print STDERR "INFO: ".join(" ",@_)."\n"; 
+}
+
+
+#  Traitement des Options
+###########################################################
+
+
+my %opts;
+getopts('hv', \%opts);
+
+my $debug_level = 0;
+$debug_level = 1 if $opts{v};
+
+usage($debug_level+1) if $opts{h};
+
+#  Traitement des arguments
+###########################################################
+
+if ( @ARGV != 2) {
+	log_info("Nombre d'argument incorrect (".@ARGV.")");
+	usage($debug_level);
+	sortie(202);
+}
+my $environ=shift;
+my $table_name=shift;
+
+#  Corps du script
+###########################################################
+my $bv_severite=0;
 use IKOS::SIP;
-
-my $environ=shift or die;
-my $table_name=shift or die;
 
 my $sip=SIP->new($environ);
 my $table=$sip->open_local_table($table_name, {debug => 0 });
