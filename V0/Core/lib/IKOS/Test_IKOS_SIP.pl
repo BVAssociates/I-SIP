5bbc87ab83e73e506f712eea4ecf754ee5c18e49
Vincent Bauchart <vincent.bauchart@laposte.net>
2008-10-27 10:45:00 +0000

    test SIP->compare_table()

--- V0/Core/lib/IKOS/Test_IKOS_SIP.pl
+++ V0/Core/lib/IKOS/Test_IKOS_SIP.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-use Test::Simple tests => 8;
+use Test::Simple tests => 9;
 use Data::Dumper;
 
 use strict;
@@ -24,10 +24,11 @@ ok($sip->exist_local_table("TEST"), "SIP->exist_local_table()");
 ok($sip->get_table_key("TEST"), "SIP->get_table_key()");
 ok(@temp=$sip->get_table_field("TEST"), "SIP->get_table_field()");
 #print Dumper @temp;
-ok($table_sqlite = $sip->open_local_table("NATPROP"), "SIP ->open_local_table()");
-ok($table_histo = $sip->open_local_from_histo_table("NATPROP"), "SIP ->open_local_from_histo_table()");
-ok($table_ikos = $sip->open_ikos_table("NATPROP"), "SIP ->open_ikos_table()");
+ok($table_sqlite = $sip->open_local_table("TEST"), "SIP->open_local_table()");
+ok($table_histo = $sip->open_local_from_histo_table("TEST"), "SIP->open_local_from_histo_table()");
+ok($table_ikos = $sip->open_ikos_table("TEST", {debug => 1} ), "SIP->open_ikos_table()");
 
 my %differences;
-ok( %differences = $sip->compare_table($table_sqlite,$table_histo),"SIP ->compare_table()" );
-print Dumper(%differences);
+ok( %differences = $sip->compare_table($table_sqlite,$table_histo),"SIP ->compare_table() bitween Sqlite and Histo" );
+ok( %differences = $sip->compare_table($table_ikos,$table_sqlite),"SIP ->compare_table() bitween ODBC and Sqlite" );
+#print Dumper(%differences);
