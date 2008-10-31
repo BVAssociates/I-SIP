e95c8fbf2bcd25ddfa98216278dafb918a178822
Vincent Bauchart <vincent.bauchart@laposte.net>
2008-10-31 10:15:45 +0000

    update script to update histo table from reference

--- V0/Core/bin/PC_UPDATE_HISTO.pl
+++ V0/Core/bin/PC_UPDATE_HISTO.pl
@@ -1,20 +1,119 @@
 #!/usr/bin/perl
 
+# Inclusions obligatoires
 use strict;
+use Pod::Usage;
+use Getopt::Std;
 
-use IKOS::Sqlite;
+#  Documentation
+###########################################################
+=head1 NAME
+
+PC_LIST_FIELD_HISTO - Liste les champs d'une table dans un environnement
+
+=head1 SYNOPSIS
+
+ PC_LIST_FIELD_HISTO.pl environnement tablename
+ 
+=head1 DESCRIPTION
+
+Liste les champs d'une table dans un environnement à la date courante
+
+=head1 ENVIRONNEMENT
+
+=item ITOOLS : L'environnement du service de l'ICles IKOS doit être chargé
+
+=head1 OPTIONS
+
+=head2 -h : Affiche l'aide en ligne
+
+=head2 -v : Mode verbeux
+
+=head1 ARGUMENTS
+
+=head2 * environnement à utiliser
+
+=head2 * table a décrire
+
+=head1 AUTHOR
+
+BV Associates, 16/10/2008
+
+=cut
 
 
-my $environ=shift or die;
-my $tablename=shift or die;
+#  Fonctions
+###########################################################
 
-# Connection to local database
-my $referentiel=Sqlite->open("IKOS_".$environ,$tablename, {debug => 1, timeout => 0});
+sub sortie ($) {
+	exit shift;
+}
 
-$|=1;
-print join(":",$referentiel->fetch_row_array)."\n";
+sub usage($) {
+	my $verbosity=shift;
+	pod2usage(-verbose => $verbosity, -noperldoc => 1);
+	sortie(202); 
+}
 
-#print $referentiel->insert_row_array( (undef) x 3, ("ok") x 8)."\n";
-my $last_id=$referentiel->insert_row( ID=> undef, ENVIRONNEMENT => $environ,  TABLE_NAME => $tablename, TABLE_KEY => "AL'TEST" ,FIELD_NAME => 10)."\n";
+sub log_erreur {
+	print STDERR "ERREUR: ".join(" ",@_)."\n"; 
+	sortie(202);
+}
 
-$referentiel->execute("DELETE from $tablename where ID = $last_id;");
+sub log_info {
+	print STDERR "INFO: ".join(" ",@_)."\n"; 
+}
+
+
+#  Traitement des Options
+###########################################################
+
+
+my %opts;
+getopts('hv', \%opts);
+
+my $debug_level = 0;
+$debug_level = 1 if $opts{v};
+
+usage($debug_level+1) if $opts{h};
+
+#  Traitement des arguments
+###########################################################
+
+if ( @ARGV < 1) {
+	log_info("Nombre d'argument incorrect (".@ARGV.")");
+	usage($debug_level);
+	sortie(202);
+}
+my $environnement=shift;
+my $table_name=shift;
+
+#  Corps du script
+###########################################################
+my $bv_severite=0;
+
+use IKOS::SIP;
+use IKOS::DATA::ITools;
+
+use POSIX qw(strftime);
+
+my $env_sip = SIP->new($environnement);
+
+my $db2_table = ITools->open("INFO_TABLE");
+
+$db2_table->query_condition("TABLE_NAME = '$table_name'") if $table_name;
+
+while (my %db2_table_line = $db2_table->fetch_row() ) {
+
+	my $table_name=$db2_table_line{TABLE_NAME};
+	
+	#open IKOS table for DATA
+	my $current_table=$env_sip->open_ikos_table($table_name, {debug => 0});
+	my $histo_table=$env_sip->open_local_from_histo_table($table_name, {debug => 1});
+	
+	#$histo_table->compare_from($current_table),
+	$histo_table->update_from($current_table),
+		
+}
+
+sortie($bv_severite);
