0aaedb0c2b62b91bc9019dc2b4f20bf56ee184a0
Vincent Bauchart <vincent.bauchart@laposte.net>
2008-11-07 14:00:47 +0000

    Add Table DATA_HISTO Add Table FIELD_INFO

---
+++ V0/Core/bin/PC_LIST_DATE.pl
@@ -0,0 +1,115 @@
+#!/usr/bin/perl
+
+# Inclusions obligatoires
+use strict;
+use Pod::Usage;
+use Getopt::Std;
+
+#  Documentation
+###########################################################
+=head1 NAME
+
+PC_LIST_DATE - Liste les date de mise à jour d'une table dans un environnement
+
+=head1 SYNOPSIS
+
+ PC_LIST_DATE.pl [-h][-v] environnement tablename
+ 
+=head1 DESCRIPTION
+
+Liste les champs d'une table dans un environnement à la date courante
+
+=head1 ENVIRONNEMENT
+
+=item ITOOLS : L'environnement du service de l'ICles IKOS doit être chargé
+
+=head1 OPTIONS
+
+=over4
+
+=item -h : Affiche l'aide en ligne
+
+=item -v : Mode verbeux
+
+=back
+
+=head1 ARGUMENTS
+
+=over4
+
+=item environnement : environnement à utiliser
+
+=item tablename : table a ouvrir
+
+=back
+
+=head1 AUTHOR
+
+BV Associates, 07/11/2008
+
+=cut
+
+
+#  Fonctions
+###########################################################
+
+sub sortie ($) {
+	exit shift;
+}
+
+sub usage($) {
+	my $verbosity=shift;
+	pod2usage(-verbose => $verbosity, -noperldoc => 1);
+	sortie(202); 
+}
+
+sub log_erreur {
+	print STDERR "ERREUR: ".join(" ",@_)."\n"; 
+	sortie(202);
+}
+
+sub log_info {
+	print STDERR "INFO: ".join(" ",@_)."\n"; 
+}
+
+
+#  Traitement des Options
+###########################################################
+
+
+my %opts;
+getopts('hv', \%opts);
+
+my $debug_level = 0;
+$debug_level = 1 if $opts{v};
+
+usage($debug_level+1) if $opts{h};
+
+#  Traitement des arguments
+###########################################################
+
+if ( @ARGV < 2) {
+	log_info("Nombre d'argument incorrect (".@ARGV.")");
+	usage($debug_level);
+	sortie(202);
+}
+my $environ=shift;
+my $table_name=shift;
+
+#  Corps du script
+###########################################################
+my $bv_severite=0;
+use IKOS::SIP;
+
+my $sip=SIP->new($environ);
+my $table=$sip->open_local_table($table_name."_HISTO", {debug => 0 });
+
+$table->query_field("DATE_HISTO");
+$table->custom_select_query("select distinct strftime('%Y/%m/%d %Hh00',DATE_HISTO) from $table_name\_HISTO");
+
+
+die "unable to open local $table_name in env $environ" if not defined $table;
+
+while (my @line=$table->fetch_row_array()) {
+	print join(',',@line)."\n";
+}
