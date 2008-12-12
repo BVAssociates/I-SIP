d59e38052d944139d835d25712ba7656c33ae04c
Vincent Bauchart <vincent.bauchart@laposte.net>
2008-12-12 14:24:39 +0000

    implement update of data in table change icon of Node after change

--- IsipProcessor/src/com/bv/isip/IsipProcessor.java
+++ IsipProcessor/src/com/bv/isip/IsipProcessor.java
@@ -8,6 +8,8 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
@@ -18,12 +20,19 @@ import com.bv.core.trace.Trace;
 import com.bv.core.trace.TraceAPI;
 import com.bv.isis.console.abs.gui.MainWindowInterface;
 import com.bv.isis.console.abs.processor.ProcessorInterface;
+import com.bv.isis.console.com.CommonFeatures;
+import com.bv.isis.console.com.LogServiceProxy;
 import com.bv.isis.console.com.ServiceSessionProxy;
 import com.bv.isis.console.common.IndexedList;
 import com.bv.isis.console.common.InnerException;
+import com.bv.isis.console.impl.processor.admin.ExecutionSurveyor;
+import com.bv.isis.console.node.GenericTreeClassNode;
 import com.bv.isis.console.node.GenericTreeObjectNode;
 import com.bv.isis.console.processor.ProcessorFrame;
+import com.bv.isis.corbacom.IsisNodeLabel;
+import com.bv.isis.corbacom.IsisTableDefinition;
 import com.bv.isis.corbacom.ServiceSessionInterface;
+import java.awt.Cursor;
 import java.util.Hashtable;
 import java.util.Iterator;
 import javax.swing.JComponent;
@@ -59,6 +68,7 @@ public class IsipProcessor extends ProcessorFrame {
 				"IsipTest", "duplicate");
 		
 		trace_methods.beginningOfMethod();
+
 		trace_methods.endOfMethod();
 		return new IsipProcessor();
 	}
@@ -103,10 +113,12 @@ public class IsipProcessor extends ProcessorFrame {
      *
      * @return le JPanel central à inserer
      */
-    protected JPanel makeFormPanel2()
+    /*
+     protected JPanel makeFormPanel2()
     {
-        return new IsipPanel();
+    return new IsipPanel();
     }
+     */
 
 
     /**
@@ -132,7 +144,9 @@ public class IsipProcessor extends ProcessorFrame {
 
             //construction du champ
             if (formType.equals("Invisible")) {
-                continue;
+                //On créé le composant, mais il ne sera pas ajouter au JPanel
+                form_value = new JLabel(formLabel);
+
             } else {
 
 
@@ -175,9 +189,9 @@ public class IsipProcessor extends ProcessorFrame {
                 constraintValue.insets = new java.awt.Insets(5, 5, 5, 5);
                 form_panel.add(form_value, constraintValue);
 
-                //on stock le champ qui sera affiché puis modifié
-                _fieldObject.put(formId, form_value);
             }
+                //on stock le champ qui sera affiché puis modifié
+            _fieldObject.put(formId, form_value);
         }
 
         return form_panel;
@@ -229,6 +243,29 @@ public class IsipProcessor extends ProcessorFrame {
 		layout.setConstraints(close_button, constraints);
 		button_panel.add(close_button);
 
+        // Maintenant, on va créer le bouton Fermer
+		JButton cancel_button =
+			new JButton("Valeurs initiales");
+		// On ajoute le callback sur le bouton
+		cancel_button.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent event)
+			{
+                try {
+                    // On appelle la méthode de fermeture
+                    populateFormPanel();
+                } catch (InnerException ex) {
+                    getMainWindowInterface().showPopupForException(
+				"Errur lors de la mise à jour", ex);
+                }
+			}
+		});
+	   constraints = new GridBagConstraints(2, 0, 1, 1, 100, 100,
+                GridBagConstraints.CENTER, GridBagConstraints.NONE,
+                new Insets(3, 0, 3, 0), 0, 0);
+		layout.setConstraints(cancel_button, constraints);
+		button_panel.add(cancel_button);
+
         return button_panel;
     }
 
@@ -258,11 +295,19 @@ public class IsipProcessor extends ProcessorFrame {
 	}
 
     private void populateFormPanel()
+            throws InnerException
     {
         //recuperation des données du noeud courant
         IsisParameter[] data=((GenericTreeObjectNode)getSelectedNode()).getObjectParameters();
 
+        boolean foundTableKey=false;
         for (int i = 0; i < data.length; i++) {
+            
+            if (data[i].name.equals("TABLE_KEY"))
+            {
+                foundTableKey = true;
+            }
+
             if (_fieldObject.containsKey(data[i].name)) {
                 JComponent textBox = _fieldObject.get(data[i].name);
                 if (textBox instanceof JTextField) {
@@ -274,17 +319,174 @@ public class IsipProcessor extends ProcessorFrame {
                 }
             }
         }
+
+        // Verification des données
+        if (!foundTableKey)
+        {
+            throw new InnerException("Execution Impossible sur ce noeud", "TABLE_KEY non defini", null);
+        }
+    }
+
+    public IsisParameter[] getFormPanelData()
+    {
+        IsisParameter[] data_from=((GenericTreeObjectNode)getSelectedNode()).getObjectParameters();
+IsisParameter[] data=new IsisParameter[data_from.length];
+ 
+        char sep=data_from[0].quoteCharacter;
+        for(int i=0; i < data_from.length; i++)
+        {
+            data[i]=new IsisParameter(data_from[i].name, data_from[i].value , sep);
+            
+            JComponent textBox = _fieldObject.get(data[i].name);
+            if (textBox instanceof JTextField) {
+                data[i].value=((JTextField) textBox).getText();
+            } else if (textBox instanceof JLabel) {
+                data[i].value=((JLabel) textBox).getText();
+            } else if (textBox instanceof JComboBox) {
+                data[i].value=(String) ((JComboBox) textBox).getSelectedItem();
+            }
+
+        }
+        return data;
     }
 
     /**
     * Elle est appelée lorsque l'utilisateur a cliqué sur le bouton "Valider".
 	*/
-    public void validateInput() {
+    public void validateInput()
+    {
+        IsisParameter[] data;
+        data = getFormPanelData();
 
+        //sablier pendant le traitement
+        getMainWindowInterface().setCurrentCursor(Cursor.WAIT_CURSOR, this);
+
+        String tableName=((GenericTreeObjectNode)getSelectedNode()).getTableName();
+        StringBuffer command = new StringBuffer();
+
+        command.append(replaceCommand+" into "+tableName +" values ");
+
+        boolean first=true;
+        for (int i = 0; i < data.length; i++) {
+            if (!first)
+            {
+                //TODO : separator from definition
+                command.append("@");
+            }
+            first = false;
+            command.append(data[i].value);
+        }
+        try {
+            execute(command.toString());
+            //on recupere les données
+            //populateFormPanel();
+            
+         } catch (InnerException ex) {
+            /*
+            getMainWindowInterface().showPopupForException(
+            "Erreur lors de l'execution de la commande", ex);
+             */
+             // parent must have already popup, so we just exit
+            return;
+        }
+        finally {
+            getMainWindowInterface().setCurrentCursor(Cursor.DEFAULT_CURSOR, this);
+        }
+
+        // changement dynamique de l'icone en cas de changement
+        GenericTreeObjectNode node= ((GenericTreeObjectNode) getSelectedNode());
+        node.getLabel().icon = "field_"+((String)((JComboBox)_fieldObject.get("STATUS")).getSelectedItem());
+        getMainWindowInterface().getTreeInterface().nodeStructureChanged(node);
+        //getMainWindowInterface().getTreeInterface().notify();
+        close();
     }
 
     /**
-     * Interroge la table STATUS et recupère les differents status
+	* Cette méthode est destinée à être appelée par les sous-classes afin
+	* d'exécuter une commande (quelle qu'elle soit).
+	* La méthode instancie un objet ExecutionSurveyor qui est réellement chargé
+	* de toute la procédure d'exécution de la commande et d'attendre la fin de
+	* celle-ci.
+	* Si une erreur est détectée, la méthode gère l'affichage d'une fenêtre
+	* indiquant la nature du problème à l'utilisateur.
+	*
+	* @param  command: Une chaîne contenant la commande à exécuter.
+	*/
+	protected void execute(
+		String command
+		) throws InnerException
+	{
+		String[] message = null;
+
+		Trace trace_methods = TraceAPI.declareTraceMethods("Console",
+			"IsipProcessor", "execute");
+		Trace trace_errors = TraceAPI.declareTraceErrors("Console");
+		Trace trace_arguments = TraceAPI.declareTraceArguments("Console");
+
+        // recuperation des informations relatives a l'execution
+        GenericTreeObjectNode selectedNode = (GenericTreeObjectNode) getSelectedNode();
+
+
+        //recuperation de l'ID'
+        String actionId =
+                LogServiceProxy.getActionIdentifier(selectedNode.getAgentName(),
+                "Mise a jour champ", null, selectedNode.getServiceName(),
+                selectedNode.getServiceType(), selectedNode.getIClesName());
+
+        trace_methods.beginningOfMethod();
+        trace_arguments.writeTrace("command=" + command);
+		// S'il n'y a pas de valeur, on sort
+		if(command == null || command.equals("") == true)
+		{
+			trace_methods.endOfMethod();
+			return;
+		}
+		// On crée un objet ExecutionSurveyor
+		ExecutionSurveyor surveyor = new ExecutionSurveyor();
+		// On lance l'exécution
+		try
+		{
+		    surveyor.execute(actionId, command, selectedNode, null);
+			// On va générer un message de log
+			message = new String[2];
+			message[0] = MessageManager.getMessage("&LOG_AdministrationCommand") +
+				command;
+			message[1] = MessageManager.getMessage("&LOG_CommandResult") +
+				MessageManager.getMessage("&LOG_CommandSuccessful");
+		}
+		catch(InnerException exception)
+		{
+			trace_errors.writeTrace(
+				"Erreur lors de l'exécution de la commande: " + command);
+			trace_errors.writeTrace("L'erreur est: " + exception.getMessage());
+			// On va afficher le message à l'utilisateur
+			getMainWindowInterface().showPopupForException(
+				"&ERR_CannotExecuteCommand", exception);
+			// On va générer un message de log
+			String[] errors =
+				CommonFeatures.buildArrayFromString(exception.getMessage());
+			message = new String[3 + errors.length];
+			int counter = 0;
+			message[counter++] =
+				MessageManager.getMessage("&LOG_AdministrationCommand") +
+				command;
+			message[counter++] = MessageManager.getMessage("&LOG_CommandResult") +
+				MessageManager.getMessage("&LOG_CommandFailed");
+			message[counter++] = MessageManager.getMessage("&LOG_CommandError");
+			for(int index = 0 ; index < errors.length ; index ++)
+			{
+				message[counter++] = errors[index];
+			}
+
+            throw exception;
+		}
+		// On génère la trace de log
+		LogServiceProxy.addMessageForAction(actionId, message);
+		trace_methods.endOfMethod();
+	}
+
+    /**
+     * Interroge la table STATUS et recupère les differents etats d'un champ
      *
      * @return Liste de Status
      */
@@ -316,5 +518,14 @@ public class IsipProcessor extends ProcessorFrame {
 	*/
 	private Hashtable<String,JComponent> _fieldObject;
 
+    /**
+     * Membre contenant une definition de la configuration permettant d'afficher
+     * les differentes boites de dialogues
+     */
     private IsipFormConfig _FormConfiguration;
+
+    /**
+     * Constante stockant la commande d'insertion
+     */
+    private final String replaceCommand="ReplaceAndExec_IKOS_FIELD.pl";
 }
