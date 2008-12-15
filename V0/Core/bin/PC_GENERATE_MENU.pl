c5c229f7fa89e3253181d6981061ef0ce2bf7add
Vincent Bauchart <vincent.bauchart@laposte.net>
2008-12-15 11:10:27 +0000

    integrate IsipProcessor in ICles

--- V0/Core/bin/PC_GENERATE_MENU.pl
+++ V0/Core/bin/PC_GENERATE_MENU.pl
@@ -149,7 +149,8 @@ my $pci_fkey_template='Item~Tables liées~Explorer~expl~~~Explore~%s~0~~Expand
 ';
 
 my $pci_field_filename="%s/IKOS_FIELD_%s.pci";
-my $pci_field_template='Item~~Historique~expl~~GSL_FILE=%s~DisplayTable~FIELD_HISTO~0~~Display';
+my $pci_field_template='Item~~Historique~expl~~GSL_FILE=%s~DisplayTable~FIELD_HISTO~0~~Display
+Item~~Editer~expl~~~IsipProcessor~~0~~Configure';
 
 my $label_table_template='IKOS_TABLE_%s.Table;key_go;Clefs de %s (%s)';
 my $label_item_template='IKOS_TABLE_%s.Item;line_%%[STATUS];%s %s';
