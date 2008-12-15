e347749eb4b35bae6d3bb022f8a4efb928a852ff
Vincent Bauchart <vincent.bauchart@laposte.net>
2008-12-15 11:07:08 +0000

    IsipFormConfig now extends SimpleSelect

--- IsipProcessor/src/com/bv/isip/IsipFormConfig.java
+++ IsipProcessor/src/com/bv/isip/IsipFormConfig.java
@@ -17,6 +17,7 @@ import com.bv.isis.corbacom.ServiceSessionInterface;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.TreeMap;
+import javax.swing.JComponent;
 
 /**
  *
@@ -27,46 +28,21 @@ import java.util.TreeMap;
  * @see IsipProcessor
  *
  */
-public class IsipFormConfig
+public class IsipFormConfig extends SimpleSelect
 {
 
     /**
-     * Constructeur prenant en parametre le noeud selectionné
+     * ouvre la table FORM_CONFIG qui contient les paramètres d'affichage
+     * du panneau de saisie
      * 
      * @param selectedNode : noeud de l'arbre en cours d'exploration
-     * @param tableName  : nom de la table qui contient la configuration
      */
-    IsipFormConfig(GenericTreeObjectNode selectedNode, String tableName) throws InnerException
+    IsipFormConfig(GenericTreeObjectNode selectedNode) throws InnerException
     {
-        super();
+        super(selectedNode,"FORM_CONFIG");
+
         Trace trace_methods = TraceAPI.declareTraceMethods("Console",
                 "IsipFormConfig", "IsipFormConfig");
-
-        // On recupere l'objet ServiceSession
-        ServiceSessionInterface service_session = selectedNode.getServiceSession();
-        IndexedList context = selectedNode.getContext(true);
-
-        // On recupere le Proxy associé
-        ServiceSessionProxy session_proxy = new ServiceSessionProxy(service_session);
-        // On va chercher les informations dans la table FORM_CONFIG
-        String[] result = session_proxy.getSelectResult(tableName, new String[] {""}, "", "", context);
-
-        // On calcule la definition à partir du Select
-        IsisTableDefinition form_definition = TreeNodeFactory.buildDefinitionFromSelectResult(result, tableName);
-
-        _table = new LinkedHashMap<String, IsisParameter[]>();
-        
-        for (int i = 1; i < result.length; i++) {
-            //On tranforme une ligne du Select en IsisParameter
-            IsisParameter[] resultLine = TreeNodeFactory.buildParametersFromSelectResult(result, i, form_definition);
-            //On recupere la valeur de la clef pour une ligne
-            String key = TreeNodeFactory.buildKeyFromSelectResult(resultLine, form_definition);
-
-            //on stock la ligne
-            _table.put(key, resultLine);
-
-        }
-
         trace_methods.endOfMethod();
     }
 
@@ -78,7 +54,7 @@ public class IsipFormConfig
      */
     public String getLabel(String key)
     {
-        return TreeNodeFactory.getValueOfParameter((IsisParameter[])get(key),"FORM_LABEL");
+        return getValue(key,"FORM_LABEL");
     }
 
     /**
@@ -89,20 +65,8 @@ public class IsipFormConfig
      */
     public String getType(String key)
     {
-        return TreeNodeFactory.getValueOfParameter((IsisParameter[])get(key),"FORM_TYPE");
+        return getValue(key,"FORM_TYPE");
     }
 
-    /*@Override*/
-    public IsisParameter[] get(String key)
-    {
-        return _table.get(key);
-    }
-
-    public Iterator keysIterator()
-    {
-        return _table.keySet().iterator();
-    }
-
-    private LinkedHashMap<String, IsisParameter[]> _table;
-
+    
 }
