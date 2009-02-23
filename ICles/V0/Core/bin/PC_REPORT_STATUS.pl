1a3314e27fb8a0d89bdb2066de722c7b070b8999
Vincent Bauchart <vincent.bauchart@laposte.net>
2009-02-23 14:48:30 +0000

    EVO implement ICON filter in REPORTs

--- ICles/V0/Core/bin/PC_REPORT_STATUS.pl
+++ ICles/V0/Core/bin/PC_REPORT_STATUS.pl
@@ -132,6 +132,15 @@ my $date_compare=$ENV{DATE_COMPARE};
 my $date_explore=$ENV{DATE_EXPLORE};
 my $module_explore=$ENV{Module} || $opts{m} || usage($debug_level);
 
+my $filter_field=$ENV{FILTER_FIELD}="ICON";
+my $filter_value=$ENV{FILTER_VALUE}="!valide";
+my $filter_exclude;
+
+if ($filter_value and $filter_value =~ /^!(.+)/) {
+	$filter_value=$1;
+	$filter_exclude=1;
+}
+
 if ( @ARGV < 1) {
 	log_info("Nombre d'argument incorrect (".@ARGV.")");
 	usage($debug_level);
@@ -168,6 +177,7 @@ my $bv_severite=0;
 use Isip::Environnement;
 use ITable::ITools;
 use Isip::ITable::DataDiff;
+use Isip::IsipRules;
 
 # New SIP Object instance
 my $env_sip = Environnement->new($environnement, {debug => $debug_level});
@@ -179,12 +189,32 @@ my $itools_table=ITools->open("FIELD_REPORT_HISTO", {debug => $debug_level});
 my $separator=$itools_table->output_separator;
 my @query_field=$itools_table->field;
 
-# Create IsipRule object
+
+# Check if it is an SQL filter
+my @comment_condition;
+if ($filter_field and $filter_field ne 'ICON' ) {
+
+	$filter_value =~ s/\*/%/g;
+	my $comp_operator;
+	if ($filter_exclude) {
+		$comp_operator='<>';
+		$comp_operator='not like' if $filter_value =~ /%/;
+	}
+	else {
+		$comp_operator='=' ;
+		$comp_operator='like' if $filter_value =~ /%/;
+	}
+	
+	# Check where is the clause
+	if ($filter_field eq 'PROJECT') {
+		push @comment_condition, "$filter_field $comp_operator '$filter_value'";
+	}
+}
 
 
 foreach my  $table_name (@table_list) {
 
-	my $rules=$env_sip->get_isip_rules($table_name);
+	my $rules=IsipRules->new($table_name);
 	my @field_order=$env_sip->get_table_field($table_name);
 	my %memory_row;
 
@@ -238,7 +268,7 @@ foreach my  $table_name (@table_list) {
 	while (my %row=$table_status->fetch_row) {
 	
 		# compute dynamic fields
-		$row{ICON}=$rules->get_field_icon($row{FIELD_NAME},$row{STATUS}, $row{COMMENT}) if exists $row{ICON} and $explore_mode eq "explore";
+		$row{ICON}=$rules->get_field_icon(%row) if exists $row{ICON} and $explore_mode eq "explore";
 			
 		$row{TYPE}=$rules->get_field_type_txt($row{FIELD_NAME}) if exists $row{TYPE};
 		$row{TEXT}=$rules->get_field_description($row{FIELD_NAME}) if exists $row{TEXT};
@@ -256,8 +286,22 @@ foreach my  $table_name (@table_list) {
 		#}
 				
 		# don't display ignored fields
-		if (not ($row{TYPE} and $row{TYPE} eq "exclus")
-				and not($row{ICON} and $row{ICON} eq "stamp")) {
+		my $display_line=1;
+		if ($filter_field eq 'ICON') {
+			if ($filter_exclude and $row{ICON} eq $filter_value) {
+				$display_line = 0;
+			}
+			elsif (not $filter_exclude and $row{ICON} ne $filter_value) {
+				$display_line = 0;
+			}
+		}
+		
+		if ($row{TYPE} and $row{TYPE} eq "exclus"
+				or $row{ICON} and $row{ICON} eq "stamp") {
+				$display_line = 0;
+		}
+
+		if ($display_line) {
 			print join($separator,map {$_} $table_status->hash_to_array(%row))."\n";
 		}
 	}
