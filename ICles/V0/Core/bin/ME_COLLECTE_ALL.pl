40dfd62e04c720eeee2ea7d24b94742c453250b8
Vincent Bauchart <vincent.bauchart@laposte.net>
2009-04-07 06:45:43 +0000

    EVO add option -p (multi-cpu) to ME_COLLECTE_ALL.pl

--- ICles/V0/Core/bin/ME_COLLECTE_ALL.pl
+++ ICles/V0/Core/bin/ME_COLLECTE_ALL.pl
@@ -15,7 +15,7 @@ ME_COLLECTE_ALL - Collecte tous les environnements
 
 =head1 SYNOPSIS
 
- ME_COLLECTE_ALL.pl [-h][-v]
+ ME_COLLECTE_ALL.pl [-h][-v][-p]
  
 =head1 DESCRIPTION
 
@@ -36,6 +36,8 @@ Collecte tous les environnements
 =item -h : Affiche l'aide en ligne
 
 =item -v : Mode verbeux
+
+=item -p : utilise un processus par environnement (fork)
 
 =back
 
@@ -79,12 +81,15 @@ sub log_info {
 
 
 my %opts;
-getopts('hv', \%opts) or usage(0);
+getopts('hvp', \%opts) or usage(0);
 
 my $debug_level = 0;
 $debug_level = 1 if $opts{v};
 
 usage($debug_level+1) if $opts{h};
+
+my $fork=$opts{p} if exists $opts{p};
+
 
 #  Traitement des arguments
 ###########################################################
@@ -106,12 +111,12 @@ require "PC_CLEAN_BASELINE.pl";
 my $config=IsipConfig->new();
 my @env_list=$config->get_environnement_list();
 
-log_screen_only();
+log_screen_only() if $fork;
 
-my $return_code;
-my $pid;
+my $return_code=0;
+my $pid=0;
 foreach my $env (@env_list) {
-	$pid = fork();
+	$pid = fork() if $fork;
 	if (!$pid) {
 		# child process
 		log_info("nettoyage avant collecte de l'environnement $env");
@@ -119,13 +124,14 @@ foreach my $env (@env_list) {
 		log_info("Collecte de l'environnement $env");
 		$return_code += pc_update_histo::run("-d",$env);
 		log_info("Terminé pour l'environnement $env");
-		last;
+		last if $fork;
 	}
 }
 
-if ($pid) {
+if ($fork and $pid) {
 	log_info("Attente que tous les process se termine");
-	$return_code=wait;
+	wait;
+	$return_code=$?;
 }
 
 exit $return_code;
