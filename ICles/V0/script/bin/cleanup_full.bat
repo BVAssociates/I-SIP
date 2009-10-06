5a3bd5ae544101b94a6a8101f2144d6b62a8c07c
Vincent Bauchart <vincent.bauchart@laposte.net>
2009-10-06 08:41:27 +0000

    EVO add script to cleanup all environnement (+defrag)

---
+++ ICles/V0/script/bin/cleanup_full.bat
@@ -0,0 +1,15 @@
+@echo OFF
+
+echo suppression cache PAR
+del /F /S /Q %TEMP%\par-%USERNAME%
+
+echo construction du profil
+perl -pe "s/^/set $1/ if !/^(#|REM|$)/; s/^#/REM /" "%~dp0\..\..\..\..\..\Portal\product\conf\IsisPortal_WIN32.ini" > "%~dp0\..\..\..\..\..\Portal\product\conf\IsisPortal_WIN32.bat"
+
+echo chargement du profil
+call "%~dp0\..\..\..\..\..\Portal\product\conf\IsisPortal_WIN32.bat"
+
+
+FOR /F %%i IN ('select -s Environnement from environ') DO (
+	PC_CLEAN_BASELINE -c %%i
+)
