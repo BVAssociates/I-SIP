89b8f5c69e3e52a03e13656f8deb91083505631a
Vincent Bauchart <vincent.bauchart@laposte.net>
2009-10-08 07:22:53 +0000

    EVO PC_EXEC_SQL.pl : change format of arguments + use script as lib

--- ICles/V0/Core/bin/PC_EXEC_SQL.pl
+++ ICles/V0/Core/bin/PC_EXEC_SQL.pl
@@ -22,14 +22,15 @@ PC_EXEC_SQL - Execute une requete SQL
 Execute une requete SQL sur toutes les tables "logiques" d'un ou tous les environnements.
 
 La requete SQL est executé sur toutes les tables des environnements. Le nom de la table physique
-est calculée à partir du nom de la table logique representée par "{}".
+est calculée à partir du nom de la table logique representée par "{tab}", et l'environnement
+par "{env}".
 
 La forme SQL "SELECT * FROM ..." peut être remplacée par "SELECT FROM ..." pour eviter les
 problèmes d'interpretations du Shell.
 
 Par exemple, pour affiche toutes les colonnes de toutes les tables :
 
- PC_EXEC_SQL.pl -t {}_COLUMN "SELECT FROM {}_COLUMN"
+ PC_EXEC_SQL.pl -t {tab}_COLUMN "SELECT '{env}',* FROM {tab}_COLUMN"
 
 =head1 ENVIRONNEMENT
 
@@ -94,94 +95,104 @@ sub log_info {
 #  Traitement des Options
 ###########################################################
 
+sub run {
+	local @ARGV=@_;
+	my $return=1;
+	# BEGIN RUN
 
-log_info("Debut du programme : ".$0." ".join(" ",@ARGV));
+	log_info("Debut du programme : ".$0." ".join(" ",@ARGV));
 
-my %opts;
-getopts('hve:t:', \%opts);
+	my %opts;
+	getopts('hve:t:', \%opts);
 
-my $debug_level = 0;
-$debug_level = 1 if $opts{v};
+	my $debug_level = 0;
+	$debug_level = 1 if $opts{v};
 
-usage($debug_level+1) if $opts{h};
+	usage($debug_level+1) if $opts{h};
 
-my $environnement=$opts{e};
-my $table_name=$opts{t} or usage($debug_level);
-
-
-#  Traitement des arguments
-###########################################################
-
-if ( @ARGV < 1 ) {
-	log_info("Nombre d'argument incorrect (".@ARGV.")");
-	usage($debug_level);
-	sortie(202);
-}
-
-my $SQL=join(' ',@ARGV);
+	my $environnement=$opts{e};
+	my $table_name=$opts{t} or usage($debug_level);
 
 
-#  Corps du script
-###########################################################
-my $bv_severite=0;
+	#  Traitement des arguments
+	###########################################################
 
-use Isip::Environnement;
-use Isip::IsipConfig;
+	if ( @ARGV < 1 ) {
+		log_info("Nombre d'argument incorrect (".@ARGV.")");
+		usage($debug_level);
+		sortie(202);
+	}
+
+	my $SQL=join(' ',@ARGV);
 
 
-my @environnement_list;
-if ($environnement) {
-	@environnement_list = $environnement;
-}
-else {
-	my $config_sip = IsipConfig->new($environnement);
-	@environnement_list = $config_sip->get_environnement_list();
-}
+	#  Corps du script
+	###########################################################
+	my $bv_severite=0;
 
-foreach my $environnement ( @environnement_list ) {
+	use Isip::Environnement;
+	use Isip::IsipConfig;
 
-	my $env = Environnement->new($environnement);
 
-	my @list_table;
-	if ( $table_name =~ /\{\}/ ) {
-		@list_table=$env->get_table_list();
+	my @environnement_list;
+	if ($environnement) {
+		@environnement_list = $environnement;
 	}
 	else {
-		@list_table=($table_name);
+		my $config_sip = IsipConfig->new($environnement);
+		@environnement_list = $config_sip->get_environnement_list();
 	}
-	
-	foreach my $logical_table (@list_table) {
-	
-		my $physical_table =  $table_name;
-		my $SQL_generated =  $SQL;
-		
-		# remplace {} par le nom logique de la table
-		$physical_table =~ s/\{\}/$logical_table/g;
-		$SQL_generated  =~ s/\{\}/$logical_table/g;
-		
-		if ( not ($env->exist_local_table($physical_table)) ) {
-			$logger->error("$physical_table n'existe pas dans $environnement");
-			next;
-		}
-		$logger->info("execution sur ${environnement}::$physical_table");
-		
-		my $table = $env->open_local_table($physical_table);
-		
-		if ( $SQL_generated =~/^SELECT/i ) {
-		
-			$SQL_generated =~ s/SELECT\s+FROM/SELECT * FROM/i;
-			
-			$table->custom_select_query($SQL_generated);
-			while ( my @result = $table->fetch_row_array() ) {
-				print join(';',@result)."\n";
-			}
+
+	foreach my $environnement ( @environnement_list ) {
+
+		my $env = Environnement->new($environnement);
+
+		my @list_table;
+		if ( $table_name =~ /\{tab\}/ ) {
+			@list_table=$env->get_table_list();
 		}
 		else {
-			my $nb = $table->execute($SQL_generated);
-			$logger->notice("$physical_table: ",$nb+0," lignes modifiés");
+			@list_table=($table_name);
+		}
+		
+		foreach my $logical_table (@list_table) {
+		
+			my $physical_table =  $table_name;
+			my $SQL_generated =  $SQL;
+			
+			# remplace {} par le nom logique de la table
+			$physical_table =~ s/\{tab\}/$logical_table/g;
+			$SQL_generated  =~ s/\{tab\}/$logical_table/g;
+			$SQL_generated  =~ s/\{env\}/$environnement/g;
+			
+			if ( not ($env->exist_local_table($physical_table)) ) {
+				$logger->error("$physical_table n'existe pas dans $environnement");
+				next;
+			}
+			$logger->info("execution sur ${environnement}::$physical_table");
+			
+			my $table = $env->open_local_table($physical_table);
+			
+			if ( $SQL_generated =~/^SELECT/i ) {
+			
+				$SQL_generated =~ s/SELECT\s+FROM/SELECT * FROM/i;
+				
+				$table->custom_select_query($SQL_generated);
+				while ( my @result = $table->fetch_row_array() ) {
+					print join(';',@result)."\n";
+				}
+			}
+			else {
+				my $nb = $table->execute($SQL_generated);
+				if ( $nb > 0) {
+					$logger->notice("$physical_table: ",$nb+0," lignes modifiés");
+				}
+			}
 		}
 	}
+	
+return $return;
+# END RUN
 }
 
-
-sortie($bv_severite);
+exit !run(@ARGV) if !caller;
