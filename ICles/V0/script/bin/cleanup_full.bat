23ebad443b50ca09fa18f740bd8a961fb0007b5e
Vincent Bauchart <vincent.bauchart@laposte.net>
2009-10-14 15:28:44 +0000

    EVO add cleanup log

--- ICles/V0/script/bin/cleanup_full.bat
+++ ICles/V0/script/bin/cleanup_full.bat
@@ -9,7 +9,8 @@ perl -pe "s/^/set $1/ if !/^(#|REM|$)/; s/^#/REM /" "%~dp0\..\..\..\..\..\Portal
 echo chargement du profil
 call "%~dp0\..\..\..\..\..\Portal\product\conf\IsisPortal_WIN32.bat"
 
-
+del "%ISIP_LOG%\isip_cleanup.log"
 FOR /F %%i IN ('select -s Environnement from environ') DO (
-	PC_CLEAN_BASELINE -c %%i
+	echo lancement du defrag sur %%i
+	PC_CLEAN_BASELINE -c %%i 2>> "%ISIP_LOG%\isip_cleanup.log"
 )
