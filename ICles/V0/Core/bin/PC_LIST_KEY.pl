96113da31c73a73129d09a366e8fc69a7f60a278
Vincent Bauchart <vincent.bauchart@laposte.net>
2010-01-22 13:59:03 +0000

    EVO avoid setting multiple foreign table on root tables

--- ICles/V0/Core/bin/PC_LIST_KEY.pl
+++ ICles/V0/Core/bin/PC_LIST_KEY.pl
@@ -15,7 +15,7 @@ PC_LIST_KEY - Liste les clefs primaire des tables
 
 =head1 SYNOPSIS
 
- PC_LIST_KEY.pl [-h][-v] [-s separateur] environnement
+ PC_LIST_KEY.pl [-h][-v] [-s separateur] [-f table] environnement
  
 =head1 DESCRIPTION
 
@@ -38,6 +38,9 @@ Liste les clefs primaire de toutes les tables dans un environnement
 =item -v : Mode verbeux
 
 =item -s : modifie le separateur de sortie
+
+=item -f table : si "table" est une table racine et quelle contient déjà une clef
+étrangère, n'affiche que les clefs primaires de celle-ci
 
 =back
 
@@ -84,9 +87,11 @@ sub log_info {
 #  Traitement des Options
 ###########################################################
 
+# quirk! because Windows leave "%VAR%" when VAR empty in args
+map {s/%\w+%//g} @ARGV;
 
 my %opts;
-getopts('hvs:', \%opts);
+getopts('hvs:f:', \%opts);
 
 my $debug_level = 0;
 $debug_level = 1 if $opts{v};
@@ -95,6 +100,9 @@ usage($debug_level+1) if $opts{h};
 
 my $separator=',';
 $separator=$opts{s} if exists $opts{s};
+
+my $table_foreign;
+$table_foreign=$opts{f} if exists $opts{f};
 
 #  Traitement des arguments
 ###########################################################
@@ -114,10 +122,33 @@ use ITable::ITools;
 
 my $env=Environnement->new($environnement);
 
-print join($separator, ('') x 2)."\n";
-foreach my $table ($env->get_table_list()) {
-	my $column_table=$env->open_local_table($table."_COLUMN");
+# recupère les tables qui ont une clef étrangère sur la table
+# restricton utile seulement sur les ROOT_TABLE
+my %parent_tables;
+if ($env->is_root_table($table_foreign) && $table_foreign) {
+	my $column_table=$env->open_local_table($table_foreign."_COLUMN");
+	
 	while (my %row=$column_table->fetch_row) {
+		if ($row{FOREIGN_TABLE}) {
+			$parent_tables{$row{FOREIGN_TABLE}}++;
+		}
+	}
+}
+
+# calcul la liste des tables
+my @table_list;
+if (%parent_tables) {
+	@table_list = keys %parent_tables;
+}
+else {
+	@table_list = $env->get_table_list();
+}
+
+# affiche les clefs primaire de la liste des tables
+print join($separator, ('') x 2)."\n";
+foreach my $table (@table_list) {
+	my $column_table=$env->open_local_table($table."_COLUMN");
+	while (my %row=$column_table->fetch_row) {		
 		print join($separator,@row{$column_table->query_field})."\n" if $row{PRIMARY_KEY};
 	}
 }
