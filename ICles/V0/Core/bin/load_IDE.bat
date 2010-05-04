b06c11d5b8104bdac8eedcaab7b8f9ed07bf972e
Vincent Bauchart <vincent.bauchart@laposte.net>
2010-05-04 11:07:43 +0000

    EVO : retire scripts inutiles

--- ICles/V0/Core/bin/load_IDE.bat
+++
@@ -1,5 +0,0 @@
-call "..\..\..\..\IKOS_test_Env.bat"
-
-
-set PERL5OPT=-w
-start "new" /B "%EDITOR%"
--- ICles/V0/Core/bin/load_eclipse.bat
+++
@@ -1,3 +0,0 @@
-call "..\..\..\..\IKOS_test_Env.bat"
-
-start "new" /B "C:\Program Files\Eclipse\eclipse.exe"
