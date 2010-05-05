f3b1404650c3735eb71c8d2849175886d349d8c8
Vincent Bauchart <vincent.bauchart@laposte.net>
2010-05-05 11:06:31 +0000

    mes à jour version et changelog depuis v1.2

--- ICles/V0/Core/bin/ME_ISIP_VERSION.pl
+++ ICles/V0/Core/bin/ME_ISIP_VERSION.pl
@@ -101,7 +101,7 @@ sub run {
 	#  Corps du script
 	###########################################################
 
-	print "I-SIP, Version 1.2-dev\n";
+	print "I-SIP, Version 1.3-dev\n";
 }
 
 exit !run(@ARGV) if !caller;
