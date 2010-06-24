66748ccb16d87ed114b6d30cb72c34a24b5d17e6
Vincent Bauchart <vincent.bauchart@laposte.net>
2010-06-24 10:50:57 +0000

    RELEASE version 1.4-dev

--- ICles/V0/Core/bin/ME_ISIP_VERSION.pl
+++ ICles/V0/Core/bin/ME_ISIP_VERSION.pl
@@ -101,7 +101,7 @@ sub run {
 	#  Corps du script
 	###########################################################
 
-	print "I-SIP, Version 1.3\n";
+	print "I-SIP, Version 1.4-dev\n";
 }
 
 exit !run(@ARGV) if !caller;
