d5402169b8897d59493ab6dcda317eba16085290
Vincent Bauchart <vincent.bauchart@laposte.net>
2010-07-06 16:40:22 +0000

    EVO IsipProcessor : - surcharge initTable et traite les arguments donné au processeur par le PCI - permet l'ajout de colonnes 'virtelles' pour permettre l'insertion de champs supplementaires (see FS#759)

--- IsipProcessor/src/com/bv/isip/EditFormProcessorIsipLine.java
+++ IsipProcessor/src/com/bv/isip/EditFormProcessorIsipLine.java
@@ -9,8 +9,14 @@ import com.bv.isis.console.core.abs.processor.ProcessorInterface;
 import com.bv.isis.console.core.common.InnerException;
 import com.bv.isis.console.node.GenericTreeObjectNode;
 import com.bv.isis.console.node.TreeNodeFactory;
+import com.bv.isis.corbacom.IsisForeignKey;
+import com.bv.isis.corbacom.IsisForeignKeyLink;
 import com.bv.isis.corbacom.IsisParameter;
+import com.bv.isis.corbacom.IsisTableColumn;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.Iterator;
 
 public class EditFormProcessorIsipLine extends EditFormProcessor {
 
@@ -82,4 +88,67 @@ public class EditFormProcessorIsipLine extends EditFormProcessor {
             throw new InnerException("Erreur lors de la modification du Label", exception.getMessage(), exception);
         }
     }
+    
+    /**
+     * Initialise le membre _tableDefinition avec la definition extraite
+     * du node en cours d'exploration
+     */
+    @Override
+    protected void initTable( String[] parameters_array )
+            throws InnerException
+    {
+        super.initTable(parameters_array);
+        
+        
+        if (parameters_array.length >= 2) {
+            
+            //modifie la définition à la volée
+            
+            ArrayList<IsisTableColumn> columns = new ArrayList<IsisTableColumn>(Arrays.asList(_tableDefinition.columns));
+            ArrayList<IsisForeignKey> fkeys = new ArrayList<IsisForeignKey>(Arrays.asList(_tableDefinition.foreignKeys));
+                        
+            String[] query_fields = parameters_array[1].split(",");
+            
+            for (int i=0; i < query_fields.length ; i++ ) {
+                
+                String field = query_fields[i];
+                
+                boolean field_found=false;
+                
+                Iterator<IsisTableColumn> iter = columns.iterator();
+                while (iter.hasNext()) {
+                    IsisTableColumn col = iter.next();
+                    
+                    if ( field.equals(col.name) ) {
+                        field_found=true;
+                    }
+                }
+                
+                if ( ! field_found ) {
+                    
+                    // SIZE="10s" par defaut
+                    //TODO : parametrer au lieu de coder en "dur"
+                    columns.add(new IsisTableColumn(field, 10, 's'));
+                    
+                    // cas spécial du STATUS
+                    //TODO : parametrer au lieu de coder en "dur"
+                    if ( field.equals("STATUS") ) {
+                        IsisForeignKeyLink link = new IsisForeignKeyLink("STATUS", "Name");
+                        fkeys.add(new IsisForeignKey("ETAT", new IsisForeignKeyLink[] {link} ));
+                    }
+                    
+                }
+            }
+            
+            // Mise à jour de la définition avec les colonnes modifiées
+            _tableDefinition.columns = columns.toArray(_tableDefinition.columns);
+            _tableDefinition.foreignKeys = fkeys.toArray(_tableDefinition.foreignKeys);
+            
+        }
+        
+        
+        
+    }
+    
+    
 }
