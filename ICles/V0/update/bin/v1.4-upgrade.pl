f4254af9292d94c55442fd98807608790d379352
Vincent Bauchart <vincent.bauchart@laposte.net>
2010-07-09 08:52:42 +0000

    EVO script de mise à jour 1.3 vers 1.4

---
+++ ICles/V0/update/bin/v1.4-upgrade.pl
@@ -0,0 +1,117 @@
+#!/usr/bin/env perl
+
+# Inclusions obligatoires
+use strict;
+use Pod::Usage;
+use Getopt::Std;
+
+use Isip::IsipLog '$logger';
+
+#  Documentation
+###########################################################
+=head1 NAME
+
+ v1.4-upgrade - Met à jour les structures de données I-SIP pour cette version
+
+=head1 SYNOPSIS
+
+ v1.4-upgrade.pl [-h][-v]
+ 
+=head1 DESCRIPTION
+
+Met à jour les structures de données I-SIP pour cette version
+
+=head1 ENVIRONNEMENT
+
+=over
+
+=item ITOOLS : L'environnement du service de l'ICles IKOS doit être chargé
+
+=back
+
+=head1 OPTIONS
+
+=over
+
+=item -h : Affiche l'aide en ligne
+
+=item -v : Mode verbeux
+
+=back
+
+=head1 ARGUMENTS
+
+
+=head1 AUTHOR
+
+Copyright (c) 2008 BV Associates. Tous droits réservés.
+
+=cut
+
+
+#  Fonctions
+###########################################################
+
+sub sortie ($) {
+	exit shift;
+}
+
+sub usage($) {
+	my $verbosity=shift;
+	pod2usage(-verbose => $verbosity, -noperldoc => 1);
+	sortie(202); 
+}
+
+sub log_erreur {
+	#print STDERR "ERREUR: ".join(" ",@_)."\n"; 
+	$logger->error(@_);
+	sortie(202);
+}
+
+sub log_info {
+	#print STDERR "INFO: ".join(" ",@_)."\n"; 
+	$logger->notice(@_);
+}
+
+
+#  Traitement des Options
+###########################################################
+
+
+my %opts;
+getopts('hv', \%opts) or usage(0);
+
+my $debug_level = 0;
+$debug_level = 1 if $opts{v};
+
+usage($debug_level+1) if $opts{h};
+
+#  Traitement des arguments
+###########################################################
+
+if ( @ARGV < 0) {
+	log_info("Nombre d'argument incorrect (".@ARGV.")");
+	usage($debug_level);
+	sortie(202);
+}
+
+
+#  Corps du script
+###########################################################
+my $bv_severite=0;
+
+use Isip::IsipConfig;
+
+my $config=IsipConfig->new();
+
+my @environnement_list=$config->get_environnement_list();
+
+foreach my $env_name (@environnement_list) {
+	# relance l'intialisation des bases de config pour ajouter les nouvelles tables
+	$config->create_database_environnement($env_name);
+	
+	# regenere les menus
+	require 'PC_GENERATE_MENU.pl';
+	pc_generate_menu::run($env_name);
+}
+
