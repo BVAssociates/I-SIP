481ecf470005780e9ff2439aba73d395697e3f93
Vincent Bauchart <vincent.bauchart@laposte.net>
2010-09-13 10:00:54 +0000

    EVO : pre-release v1.5

--- ICles/V0/Core/bin/ME_ISIP_VERSION.pl
+++ ICles/V0/Core/bin/ME_ISIP_VERSION.pl
@@ -101,7 +101,7 @@ sub run {
 	#  Corps du script
 	###########################################################
 
-	print "I-SIP, Version 1.4\n";
+	print "I-SIP, Version 1.5-dev\n";
 }
 
 exit !run(@ARGV) if !caller;
