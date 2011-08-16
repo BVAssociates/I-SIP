389ec6dc429a6b655b9b0d1aea5f70974b4bbb9e
Vincent Bauchart <vincent.bauchart@laposte.net>
2011-08-16 13:35:18 +0000

    EVO add PC_PURGE_FILES in cleanup_full.bat

--- ICles/V0/script/bin/cleanup_full.bat
+++ ICles/V0/script/bin/cleanup_full.bat
@@ -1,7 +1,7 @@
 @echo OFF
 
-echo suppression cache PAR
-del /F /S /Q %TEMP%\par-%USERNAME%
+REM echo suppression cache PAR
+REM del /F /S /Q %TEMP%\par-%USERNAME%
 
 echo construction du profil
 perl -pe "s/^/set $1/ if !/^(#|REM|$)/; s/^#/REM /" "%~dp0\..\..\..\..\..\Portal\product\conf\IsisPortal_WIN32.ini" > "%~dp0\..\..\..\..\..\Portal\product\conf\IsisPortal_WIN32.bat"
@@ -14,3 +14,5 @@ FOR /F %%i IN ('select -s Environnement from environ') DO (
 	echo lancement du defrag sur %%i
 	PC_CLEAN_BASELINE -c %%i 2>> "%ISIP_LOG%\isip_cleanup.log"
 )
+
+PC_PURGE_FILES -d 120
